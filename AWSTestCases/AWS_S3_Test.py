a0c7496b38b096f31bb906f9683a6c60704262f2
vivekgajbe <vpgajbe1991@gmail.com>
2024-08-30 11:59:54 +0530

    make test case success

--- AWSTestCases/AWS_S3_Test.py
+++ AWSTestCases/AWS_S3_Test.py
@@ -12,3 +12,18 @@ def aws_s3_check_if_buckets_exist(s3):
             return True
         else :
             return False
+        
+        def aws_s3_check_if_buckets_file_exist(s3):
+    try:
+      response = s3.list_buckets()
+      buckets =[]
+      for bucket in response['Buckets'] :
+        buckets += {bucket["Name"]}
+    except Exception as e:
+        print("error occured while invoking aws_s3_check_if_buckets_exist and error is {}".format(e))
+        return False
+    else:
+        if buckets :
+            return True
+        else :
+            return False
